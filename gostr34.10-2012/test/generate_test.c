e9ce6db660a2b7d5ee93e5bca8903c85e7fef145
knikulov <nateford@inbox.ru>
2013-03-19 08:43:55 +0400

    modified:   Makefile 	new file:   man/man3/Makefile 	new file:   man/man3/geds_context_free.man 	new file:   man/man3/geds_context_new.man 	new file:   man/man3/geds_point_free.man 	new file:   man/man3/geds_point_new.man 	new file:   man/man3/geds_set_length.man 	new file:   man/man3/geds_set_point.man 	modified:   test/generate_test.c

--- gostr34.10-2012/test/generate_test.c
+++ gostr34.10-2012/test/generate_test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#include <mpl.h>
 
 #include "geds.h"
 
